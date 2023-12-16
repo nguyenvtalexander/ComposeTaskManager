fbeec4c50c8fdbdc75413ea3701da947f653af93
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-12-16 11:46:54 -0800

    Added resources, composables, and styled

--- app/src/main/java/com/example/composetaskmanager/MainActivity.kt
+++ app/src/main/java/com/example/composetaskmanager/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.composetaskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composetaskmanager.ui.theme.ComposeTaskManagerTheme
 
@@ -22,7 +29,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TaskManager(
+                        stringResource(R.string.all_tasks_completed),
+                        stringResource(R.string.nice_work)
+                    )
                 }
             }
         }
@@ -30,17 +40,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun TaskManager(title: String, subtitle: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.ic_task_completed)
+    Column(
+        modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        TaskManagerText(
+            title,
+            subtitle,
+            modifier
+        )
+    }
+}
+
+@Composable
+fun TaskManagerText(title: String, subtitle: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = title,
+        fontWeight = FontWeight.Bold,
+        modifier = modifier
+    )
+    Text(
+        text = subtitle,
         modifier = modifier
     )
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
-fun GreetingPreview() {
+fun TaskManagerPreview() {
     ComposeTaskManagerTheme {
-        Greeting("Android")
+        TaskManager(
+            stringResource(R.string.all_tasks_completed),
+            stringResource(R.string.nice_work)
+        )
     }
 }
